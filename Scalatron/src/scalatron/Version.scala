d6b2a79760c77c4dadd068cbf746c2c9e230748b
Gerd Waldmann <gerd.waldmann@zalando.de>
2016-12-19 17:15:14 +0100

    null check replaced by Optional

--- Scalatron/src/scalatron/Version.scala
+++ Scalatron/src/scalatron/Version.scala
@@ -6,8 +6,6 @@ package scalatron
 object Version {
   val VersionString = {
     // if we are not running from a .jar file (e.g. in debugger from .class files), the resource query may return 'null'
-    val versionFromResource =
-      Version.getClass.getPackage.getImplementationVersion
-    if (versionFromResource == null) "unknown" else versionFromResource
+    Option(Version.getClass.getPackage.getImplementationVersion).getOrElse("unknown")
   }
 }
