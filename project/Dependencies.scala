3163a943945750b127f093a74d3c0e79b07e5929
JCranky <paulo.siqueira@gmail.com>
2016-12-20 10:04:24 +0100

    Migrate all tests to specs2
    closes #4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,29 +2,29 @@ import sbt._
 import Versions._
 
 object Dependencies {
+  val specs2 = Seq(
+    "org.specs2" %% "specs2-core"  % specs2Version % Test
+  )
+
   val cli = Seq(
-    "org.apache.httpcomponents" % "httpclient"                % HttpClient,
-    "org.scala-lang.modules"    %% "scala-parser-combinators" % ParserCombinators
+    "org.apache.httpcomponents" % "httpclient"                % httpClientVersion,
+    "org.scala-lang.modules"    %% "scala-parser-combinators" % parserCombinatorsVersion
   )
 
   def scalatron(scalaVersion: String) = Seq(
     "org.scala-lang"              % "scala-compiler"               % scalaVersion,
-    "org.eclipse.jetty.aggregate" % "jetty-webapp"                 % Jetty intransitive (),
-    "com.fasterxml.jackson.jaxrs" % "jackson-jaxrs-json-provider"  % Jackson,
-    "com.sun.jersey"              % "jersey-bundle"                % Jersey exclude ("javax.ws.rs", "jsr311-api"),
-    "javax.servlet"               % "servlet-api"                  % ServletApi,
-    "org.eclipse.jgit"            % "org.eclipse.jgit"             % JGit,
-    "org.eclipse.jgit"            % "org.eclipse.jgit.http.server" % JGit,
-    "org.scalatest"               %% "scalatest"                   % Scalatest % Test,
-    "org.specs2"                  %% "specs2-core"                 % Specs2    % Test
-  )
+    "org.eclipse.jetty.aggregate" % "jetty-webapp"                 % jettyVersion intransitive (),
+    "com.fasterxml.jackson.jaxrs" % "jackson-jaxrs-json-provider"  % jacksonVersion,
+    "com.sun.jersey"              % "jersey-bundle"                % jerseyVersion exclude ("javax.ws.rs", "jsr311-api"),
+    "javax.servlet"               % "servlet-api"                  % servletApiVersion,
+    "org.eclipse.jgit"            % "org.eclipse.jgit"             % jGitVersion,
+    "org.eclipse.jgit"            % "org.eclipse.jgit.http.server" % jGitVersion
+  ) ++ specs2
 
-  val markdown = Seq(
-    "org.scalatest" %% "scalatest" % Scalatest % Test
-  )
+  val markdown = specs2
 
   val core = Seq(
-    "com.typesafe.akka" %% "akka-actor" % Akka
+    "com.typesafe.akka" %% "akka-actor" % akkaVersion
   )
 
   val botWar = Seq.empty[ModuleID]
